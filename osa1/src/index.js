516d5c890f53705c8653765d42b1c97c9d81703c
Your Name <you@example.com>
2018-01-17 01:18:40 +0200

    jee

--- osa1/src/index.js
+++ osa1/src/index.js
@@ -1,27 +1,51 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-const App = () => {
-  const kurssi = 'Half Stack -sovelluskehitys'
-  const osa1 = 'Reactin perusteet'
-  const tehtavia1 = 10
-  const osa2 = 'Tiedonvälitys propseilla'
-  const tehtavia2 = 7
-  const osa3 = 'Komponenttien tila'
-  const tehtavia3 = 14
+const Otsikko = (props) => {
+    return (
+        <div>
+            <h1>{props.otsikko}</h1>
+        </div>
+    )
+}
 
-  return (
-    <div>
-      <h1>{kurssi}</h1>
-      <p>{osa1} {tehtavia1}</p>
-      <p>{osa2} {tehtavia2}</p>
-      <p>{osa3} {tehtavia3}</p>
-      <p>yhteensä {tehtavia1 + tehtavia2 + tehtavia3} tehtävää</p>
-    </div>
-  )
+const Yhteensa = (props) => {
+    return (
+        <div>
+            <p>yhteensa {props.tehtavaa} tehtävää</p>
+        </div>
+    )
+}
+
+const Sisalto = (props) => {
+    return (
+        <div>
+            <p>{props.osa} {props.tehtavia}</p>
+        </div>
+    )
+}
+
+const App = () => {
+    const kurssi = 'Half Stack -sovelluskehitys'
+    const osa1 = 'Reactin perusteet'
+    const tehtavia1 = 10
+    const osa2 = 'Tiedonvälitys propseilla'
+    const tehtavia2 = 7
+    const osa3 = 'Komponenttien tila'
+    const tehtavia3 = 14
+
+    return (
+        <div>
+            <Otsikko otsikko={kurssi} />
+            <Sisalto osa={osa1} tehtavia={tehtavia1} />
+            <Sisalto osa={osa2} tehtavia={tehtavia2} />
+            <Sisalto osa={osa3} tehtavia={tehtavia3} />
+            <Yhteensa tehtavaa={tehtavia1 + tehtavia2 + tehtavia3} />
+        </div>
+    )
 }
 
 ReactDOM.render(
-  <App />,
-  document.getElementById('root')
+    <App />,
+    document.getElementById('root')
 )
