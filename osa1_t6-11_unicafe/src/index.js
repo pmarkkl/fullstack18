83cf3c2b17ab241b2f2df5f48621b6b99615d705
Your Name <you@example.com>
2018-01-17 01:50:38 +0200

    jee

---
+++ osa1_t6-11_unicafe/src/index.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const App = () => {
+
+    return (
+        <div>
+            <h1>anna palautetta Unicafelle</h1>
+        </div>
+  )
+}
+
+ReactDOM.render(
+    <App />,
+    document.getElementById('root')
+)
